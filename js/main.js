16abd3d2772f289e403b1a3f15cec3186e5df3d3
Jack Nichols <jacknichols_88@yahoo.co.uk>
2019-11-22 11:21:07 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+// NAV BAR SCROLL
 var nav = document.getElementById('navBar');
 
 window.addEventListener('scroll', function (e) {
     var nav = document.getElementById('navBar');
     if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
-            nav.classList.add('header-scrolled', 'header-scrolled__link');
-            nav.classList.remove('header', 'header__link');
+            nav.classList.add('header-scrolled');
+            nav.classList.remove('header');
+            document.querySelector('.header__logo').style.display = 'none';
+            document.querySelector('.header-scrolled__logo').style.display = 'block';
+            document.querySelector('.header__list').style.display = 'none';
+            document.querySelector('.header-scrolled__list').style.display = 'block';
+            document.querySelector('.social').style.top = '22px';
+
+
         } else {
-            nav.classList.add('header', 'header__link');
-            nav.classList.remove('header-scrolled', 'header-scrolled__link');
+            nav.classList.add('header');
+            nav.classList.remove('header-scrolled');
+            document.querySelector('.header__logo').style.display = 'block';
+            document.querySelector('.header-scrolled__logo').style.display = 'none';
+            document.querySelector('.header__list').style.display = 'block';
+            document.querySelector('.header-scrolled__list').style.display = 'none';
+            document.querySelector('.social').style.top = '30px';
+
         }
 });
