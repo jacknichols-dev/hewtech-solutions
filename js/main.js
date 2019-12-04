ea064f53ddc98f6b899370168fe00e7f235a0e83
Jack Nichols <jacknichols_88@yahoo.co.uk>
2019-12-04 11:47:39 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -58,3 +58,64 @@ $("#btnTop").click(function() {
   }, 600);
 });
 
+
+/*** 
+********** Services header Parallax ***********
+***/
+
+var velocity = 0.2;
+
+function update(){ 
+var pos = $(window).scrollTop(); 
+$('.services__header--bg').each(function() { 
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height()-30;
+    $(this).css('backgroundPosition', '90% ' + Math.round((height - pos) * velocity) +  'px'); 
+   }); 
+   };
+
+ $(window).bind('scroll', update);
+
+
+
+ /*** 
+********** HERO ROTATING TEXT ***********
+***/
+
+var words = document.querySelectorAll(".word");
+words.forEach(function (word) {
+    var letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach(function (letter) {
+        var span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+var currentWordIndex = 0;
+var maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+var rotateText = function () {
+    var currentWord = words[currentWordIndex];
+    var nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+    // rotate out letters of current word
+    Array.from(currentWord.children).forEach(function (letter, i) {
+        setTimeout(function () {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+    // reveal and rotate in letters of next word
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach(function (letter, i) {
+        letter.className = "letter behind";
+        setTimeout(function () {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+    currentWordIndex =
+        currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+rotateText();
+setInterval(rotateText, 4000);
