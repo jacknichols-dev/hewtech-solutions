f231b6768184455d595ab1bbb39e33b96aeba958
Jack Nichols <jacknichols_88@yahoo.co.uk>
2020-01-12 08:40:56 +0000

    update

--- js/key.js
+++
@@ -1,8 +0,0 @@
-function initMap() {
-  let options = {
-    zoom: 8,
-    center: { lat: 53.4808, lng: -2.2426 }
-  };
-
-  let map = new google.maps.Map(document.getElementById("map"), options);
-}
